cabebc9340bfb084adbd794cece20e707c470d82
Suriya-Haidari <soriahaidary17@gmail.com>
2025-02-18 12:05:44 -0800

    Initial commit

---
+++ assignment.js
@@ -0,0 +1,14 @@
+//  write a program that takes a number as input and check if it is odd or even 
+
+function checkNum(num) {
+
+    if (num % 2 == 0) {
+        console.log("The number is even!")
+    } else {
+        console.log("The number is odd!")
+    }
+    return 
+}
+
+checkNum(4) // the number is even!
+
