1bfb35acc561ae7ddf8524c8e1629ad1463d9146
Suriya-Haidari <soriahaidary17@gmail.com>
2025-03-09 23:39:37 -0700

    Data structures

--- Data Structures (Next 2 Weeks)/data_structures.js
+++ Data Structures (Next 2 Weeks)/data_structures.js
@@ -4,6 +4,7 @@
 
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+
 function findLargestElement() {
 
     let largest = array[0];
@@ -111,4 +112,3 @@ function reverseArray(arr) {
   
   console.log(reverseArray([1,2,3]));
 
-  
