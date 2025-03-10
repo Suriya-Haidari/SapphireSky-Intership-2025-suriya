f0b4bd6295f27f4fe9696324752542815f522064
Suriya-Haidari <soriahaidary17@gmail.com>
2025-03-10 01:42:53 -0700

    New changes

--- Programming Basics (First 2 Weeks)/programming-basics.js
+++ Programming Basics (First 2 Weeks)/programming-basics.js
@@ -1,3 +1,6 @@
+// Find GCD (Greatest Common Divisor)
+// Given two numbers, find their GCD using a loop.
+
 function findGCD(a, b) {
     let gcd = 1;
   
@@ -38,3 +41,31 @@ function printPyramid(n) {
   
   printPyramid(4);
   
+
+// Convert Celsius to Fahrenheit
+// Write a function celsiusToFahrenheit(celsius) that converts a temperature from Celsius to Fahrenheit using the formula:
+// F=(C×95)+32F = (C \times \frac{9}{5}) + 32F=(C×59​)+32
+
+function celsiusToFahrenheit(celsius) {
+    // Formula to convert Celsius to Fahrenheit
+    let fahrenheit = (celsius * 9/5) + 32;
+    return fahrenheit;
+}
+
+console.log("Celsius" +" "+ celsiusToFahrenheit(100)); 
+
+
+// Find the Maximum of Three Numbers
+// Implement a function maxOfThree(a, b, c) that returns the largest of three numbers.
+function maxOfThree(a, b, c) {
+    if (a >= b && a >= c) {
+        return a;
+    } else if (b >= a && b >= c) {
+        return b;
+    } else {
+        return c;
+    }
+}
+
+console.log(maxOfThree(10, 20, 15)); 
+console.log(maxOfThree(5, 8, 12));
